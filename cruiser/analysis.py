07702a34cb3adc6105c2b19f6f70a750c4811623
Anthony Scopatz <scopatz@gmail.com>
2017-11-05 20:17:04 -0500

    Added build schedule

--- cruiser/analysis.py
+++ cruiser/analysis.py
@@ -25,6 +25,32 @@ def plot_fco_egen(evaler=None):
     plt.title('Energy Generated')
 
 
+def plot_fco_umined(evaler=None):
+    evaler = EVALER if evaler is None else evaler
+    frame = evaler.eval('FcoUMined')
+    fig = plt.figure()
+    plt.plot(frame['Year'], frame['UMined'])
+    plt.xlabel('Years')
+    plt.ylabel('Uranium Mined [tonnes]')
+    plt.title('Uranium Mined')
+
+
+def plot_builds(evaler=None):
+    evaler = EVALER if evaler is None else evaler
+    frame = evaler.eval('BuildSeries')
+    fig = plt.figure()
+    protos = sorted(set(frame['Prototype']))
+    width = 1.0 / len(protos)
+    for i, proto in enumerate(protos):
+        mask = frame['Prototype'] == proto
+        plt.bar(frame['EnterTime'][mask]/12.0 + i*width, frame['Count'][mask],
+                label=proto, width=width)
+    plt.xlabel('Years')
+    plt.ylabel('Count')
+    plt.title('Number of Facilities Built')
+    plt.legend(loc=0)
+
+
 def stock_analysis(filename):
     """Runs stock analysis utilities."""
     global EVALER
@@ -33,5 +59,7 @@ def stock_analysis(filename):
     FIGS.clear()
     db = cym.dbopen(filename)
     evaler = EVALER = cym.Evaluator(db)
-    display(plot_fco_egen())
+    plot_fco_egen()
+    #plot_fco_umined()
+    plot_builds()
     #db.close()
